41213c1bf333a284ed00dde36c75a2dc7d207718
cm5761 <98443200+cm5761@users.noreply.github.com>
2023-08-10 03:32:55 -0400

    Update based on latest version of SampleNetworkClient.py

--- testing_ground/SampleNetworkClient_Cope.py
+++ testing_ground/SampleNetworkClient_Cope.py
@@ -1,24 +1,63 @@
+# switched to TCP from UDP in preparation for TLS
+# rate limits
+# hashicorp vault for token
+# error handling for server results and socket
+
+"""
+modified the getTemperatureFromPort and authenticate methods to return None on error conditions. This helps in handling errors gracefully and prevents crashes due to unexpected exceptions."""
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 import math
 import socket
 
-#changes:
-# switched to TCP from UDP in preparation for TLS
+import hvac
+import sys
+from functools import wraps
 
-class SimpleNetworkClient :
-    def __init__(self, port1, port2) :
+from run_vault import get_stored_token
+from run_vault import create_vault_client
+
+import ssl
+
+# Vulnerability 1 rate limiting global constant
+RATE_LIMIT = 100  # per second
+
+# Vulnerability 1 we provide a wrapper around functions that contact the server to limit them to maximum RATE_MINUTE calls per second
+def rate_limited(max_per_second):
+    min_interval = 1.0 / float(max_per_second)
+
+    def decorate(func):
+        last_time_called = [0.0]
+
+        @wraps(func)
+        def rate_limited_function(*args, **kwargs):
+            elapsed = time.perf_counter() - last_time_called[0]
+            left_to_wait = min_interval - elapsed
+            if left_to_wait > 0:
+                time.sleep(left_to_wait)
+            ret = func(*args, **kwargs)
+            last_time_called[0] = time.perf_counter()
+            return ret
+
+        return rate_limited_function
+
+    return decorate
+
+
+class SimpleNetworkClient:
+    def __init__(self, port1, port2):
         self.fig, self.ax = plt.subplots()
         now = time.time()
         self.lastTime = now
-        self.times = [time.strftime("%H:%M:%S", time.localtime(now-i)) for i in range(30, 0, -1)]
-        self.infTemps = [0]*30
-        self.incTemps = [0]*30
+        self.times = [time.strftime("%H:%M:%S", time.localtime(now - i)) for i in range(30, 0, -1)]
+        self.infTemps = [0] * 30
+        self.incTemps = [0] * 30
         self.infLn, = plt.plot(range(30), self.infTemps, label="Infant Temperature")
         self.incLn, = plt.plot(range(30), self.incTemps, label="Incubator Temperature")
         plt.xticks(range(30), self.times, rotation=45)
-        plt.ylim((20,50))
+        plt.ylim((20, 50))
         plt.legend(handles=[self.infLn, self.incLn])
         self.infPort = port1
         self.incPort = port2
@@ -26,64 +65,99 @@ class SimpleNetworkClient :
         self.infToken = None
         self.incToken = None
 
-        self.ani = animation.FuncAnimation(self.fig, self.updateInfTemp, interval=500, save_count=30)
-        self.ani2 = animation.FuncAnimation(self.fig, self.updateIncTemp, interval=500, save_count=30)
+        self.ani = animation.FuncAnimation(self.fig, self.updateInfTemp, interval=500)
+        self.ani2 = animation.FuncAnimation(self.fig, self.updateIncTemp, interval=500)
 
-    def updateTime(self) :
+        # Set up the Vault client
+        self.vault_client = create_vault_client()
+
+    def updateTime(self):
         now = time.time()
-        if math.floor(now) > math.floor(self.lastTime) :
+        if math.floor(now) > math.floor(self.lastTime):
             t = time.strftime("%H:%M:%S", time.localtime(now))
             self.times.append(t)
-            #last 30 seconds of of data
+            # last 30 seconds of data
             self.times = self.times[-30:]
             self.lastTime = now
-            plt.xticks(range(30), self.times,rotation = 45)
+            plt.xticks(range(30), self.times, rotation=45)
             plt.title(time.strftime("%A, %Y-%m-%d", time.localtime(now)))
 
-    def getTemperatureFromPort(self, p, tok) :
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(("127.0.0.1", p))
-            s.sendall(b"%s;GET_TEMP" % tok)
-            msg = s.recv(1024)
-        m = msg.decode("utf-8")
-        return (float(m))
+    @rate_limited(RATE_LIMIT)
+    def getTemperatureFromPort(self, p, tok):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+                context.check_hostname = False  # Disable hostname verification - ONLY BECAUSE WE DO NOT HAVE A SIGNED CERT TO USE
+                context.verify_mode = ssl.CERT_NONE  # Disable certificate verification - ONLY BECAUSE WE DO NOT HAVE A SIGNED CERT TO USE
+                ssl_socket = context.wrap_socket(s, server_hostname="127.0.0.1")
+                ssl_socket.connect(("127.0.0.1", p))
+                ssl_socket.sendall(b"%s;GET_TEMP" % tok)
+                msg = ssl_socket.recv(1024)
+            m = msg.decode("utf-8")
+            try:
+                temperature = float(m)
+                return temperature
+            except ValueError:
+                print("Received non-numeric value:", m)
+                return None  # Vulnerability 3 error handling: Return None on error
+        except socket.error as e:
+            print("Socket error:", e)
+            return None  # Vulnerability 3 error handling: Return None on error
 
-    def authenticate(self, p, pw) :
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(("127.0.0.1", p))
-            s.sendall(b"AUTH %s" % pw)
-            msg = s.recv(1024)
+    @rate_limited(RATE_LIMIT)
+    def authenticate(self, p, pw):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+                context.check_hostname = False  # Disable hostname verification - ONLY BECAUSE WE DO NOT HAVE A SIGNED CERT TO USE
+                context.verify_mode = ssl.CERT_NONE  # Disable certificate verification - ONLY BECAUSE WE DO NOT HAVE A SIGNED CERT TO USE
+                ssl_socket = context.wrap_socket(s, server_hostname="127.0.0.1")
+                ssl_socket.connect(("127.0.0.1", p))
+                ssl_socket.sendall(b"AUTH %s" % pw)
+                msg = ssl_socket.recv(1024)
 
-            response = msg.strip().decode("utf-8")
-            if response.startswith("AUTH"):
-              if p == self.infPort:
-                self.infToken = response.split()[1]
-              else:
-                self.incToken = response.split()[1]
-            else:
-              return msg.strip()
+                if msg.startswith(b"TOKEN"):
+                    self.token = msg.split(b" ")[1].decode("utf-8")
+                    print("Authentication Success!")
+                else:
+                    print("Authentication Failed!")                                       
+            #return msg.strip()
+        except socket.error as e:
+            print("Socket error:", e)
+            return None  # Vulnerability 3 error handling: Return None on error
 
-    def updateInfTemp(self, frame) :
+    def updateInfTemp(self, frame):
         self.updateTime()
-        if self.infToken is None : #not yet authenticated
-            self.infToken = self.authenticate(self.infPort, b"!Q#E%T&U8i6y4r2w")
+        if self.infToken is None:  # not yet authenticated
+            self.infToken = self.authenticate(self.infPort, get_stored_token(self.vault_client).encode('utf-8'))
 
-        self.infTemps.append(self.getTemperatureFromPort(self.infPort, self.infToken)-273)
-        #self.infTemps.append(self.infTemps[-1] + 1)
+        temperature = self.getTemperatureFromPort(self.infPort, self.infToken)
+        
+        if temperature is not None and isinstance(temperature, float):
+            self.infTemps.append(temperature - 273)
+        else:
+            print("Discarded non-float temperature:", temperature) # Vulnerability 3 error handling for rate limiting
+
         self.infTemps = self.infTemps[-30:]
         self.infLn.set_data(range(30), self.infTemps)
         return self.infLn,
 
-    def updateIncTemp(self, frame) :
+    def updateIncTemp(self, frame):
         self.updateTime()
-        if self.incToken is None : #not yet authenticated
-            self.incToken = self.authenticate(self.incPort, b"!Q#E%T&U8i6y4r2w")
+        if self.incToken is None:  # not yet authenticated
+            self.incToken = self.authenticate(self.incPort, get_stored_token(self.vault_client).encode('utf-8'))
 
-        self.incTemps.append(self.getTemperatureFromPort(self.incPort, self.incToken)-273)
-        #self.incTemps.append(self.incTemps[-1] + 1)
+        temperature = self.getTemperatureFromPort(self.incPort, self.incToken)
+        
+        if temperature is not None and isinstance(temperature, float):
+            self.incTemps.append(temperature - 273)
+        else:
+            print("Discarded non-float temperature:", temperature) # Vulnerability 3 error handling for rate limiting
+
         self.incTemps = self.incTemps[-30:]
         self.incLn.set_data(range(30), self.incTemps)
         return self.incLn,
+
 
 snc = SimpleNetworkClient(23456, 23457)
 
