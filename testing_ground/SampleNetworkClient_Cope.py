2b485817c4b958a5b564366e502fee3f234273fa
cm5761 <98443200+cm5761@users.noreply.github.com>
2023-08-11 17:07:54 -0400

    adjust temp range to be in kelvin, as temperature is coming in as Kelvin

--- testing_ground/SampleNetworkClient_Cope.py
+++ testing_ground/SampleNetworkClient_Cope.py
@@ -150,7 +150,7 @@ class SimpleNetworkClient:
             temperature = self.getTemperatureFromPort(self.infPort, self.infToken["token"])
 
             if temperature is not None and isinstance(temperature, float):
-                if 34 <= temperature <= 38: #Infant temp range in Celcius
+                if 307 <= temperature <= 311: #Infant temp range in Celcius
                     self.infTemps.append(temperature - 273)
                 else: 
                     print("Temp is out of range (Inf):", temperature)
@@ -177,7 +177,7 @@ class SimpleNetworkClient:
             temperature = self.getTemperatureFromPort(self.incPort, self.incToken["token"])
 
             if temperature is not None and isinstance(temperature, float):
-                if 15.6 <= temperature <= 39: #Incubator air temp range in Celcius
+                if 289 <= temperature <= 312: #Incubator air temp range in Celcius
                     self.incTemps.append(temperature - 273)
                 else: 
                     print("Temp is out of range (Inc):", temperature)
